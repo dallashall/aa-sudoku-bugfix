ef6b2277a6e34aa27a59ab28b13239b65581b729
App Academy Student <student@appacademy.io>
2017-01-26 14:28:07 -0800

    Adds SudokuGame#parse_pos

--- sudoku.rb
+++ sudoku.rb
@@ -1,8 +1,6 @@
 require_relative "board"
 require 'colorize'
 
-puts "Only contractors write code this bad.".yellow
-
 class SudokuGame
   def self.from_file(filename)
     board = Board.from_file(filename)
@@ -10,7 +8,7 @@ class SudokuGame
   end
 
   def initialize(board)
-    @board = [[]]
+    @board = board
   end
 
   def method_missing(method_name, *args)
@@ -33,12 +31,16 @@ class SudokuGame
       rescue
         # TODO: Google how to print the error that happened inside of a rescue statement.
         puts "Invalid position entered (did you use a comma?)"
-        puts ""
+        puts "#{$!}"
 
         pos = nil
       end
     end
     pos
+  end
+
+  def parse_pos(string)
+    string.split(",").map(&:to_i)
   end
 
   def get_val
@@ -85,3 +87,4 @@ end
 
 
 game = SudokuGame.from_file("puzzles/sudoku1.txt")
+game.run
